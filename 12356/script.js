f15ebe10bf1c7abe10683591fc54bd1ac79088f4
cswideezz <162716736+cswideezz@users.noreply.github.com>
2024-03-11 18:04:59 +0500

    first comm

---
+++ 12356/script.js
@@ -0,0 +1,44 @@
+// 2 задание
+// const result = Math.pow(5, 2);
+// console.log(result);
+
+//3
+// function culc(num1,num2,math){
+//     if(math === "+"){
+//         console.log (num1+num2)
+//     }
+//     if(math)
+// }
+
+//4
+// function numCheck(num) {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return 'Число непростое';
+//     }
+//   }
+//   return 'Число простое';}
+// let num = +prompt('Введите число');
+// alert(numCheck(num));
+
+// 5
+// function showMessage(num) {
+//    let res;
+//    for (let i = 2; i < 11; i++) {
+//       res = num + ' * ' + i + ' = ' + (num * i);
+//       console.log(res);
+//    }
+// }
+// let num = +prompt('Введите число от 2 до 9');
+
+// showMessage(num);
+
+// 9. Задание.
+
+// function showEvenOdd(minNum, maxNum, bool) {
+
+// }
+
+let minNum = +prompt('Минимальное значение диапазона');
+let maxNum = +prompt('Минимальное значение диапазона');
+let bool = confirm('Вывести четные(Да) или нечетные(Нет) числа');
